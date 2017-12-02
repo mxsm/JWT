8af43a21195d9465fae33e1dad472b76dc02a4c6
AntAndElephant <ljbmxsm@163.com>
2017-12-02 12:13:22 +0800

    Initial commit

---
+++ src/main/java/mxsm/github/jwt/App.java
@@ -0,0 +1,13 @@
+package mxsm.github.jwt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
